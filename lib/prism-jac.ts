6a6c8997fe56d3036f71c4233ee8cf2583543a83
RanuriG <ranurigurusinghe@gmail.com>
2025-09-15 19:15:44 +0530

    changed syntax

--- lib/prism-jac.ts
+++ lib/prism-jac.ts
@@ -1,31 +1,53 @@
 import Prism from 'prismjs';
 
 Prism.languages.jac = {
-    'comment': [
-        {
-            pattern: /(^|[^#])\/\*[\s\S]*?(?:\*\/|$)/,
-            lookbehind: true,
-            greedy: true
-        },
-        {
-            pattern: /(^|[^#:])\/\/.*/,
-            lookbehind: true,
-            greedy: true
-        }
-    ],
-    'string': {
-        pattern: /("|')(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
-        greedy: true
+  // Comments: Python-style (#...), and Jac block (#* ... *#)
+  'comment': [
+    {
+      pattern: /#\*[\s\S]*?\*#/,
+      greedy: true
     },
-    'class-name': {
-        pattern: /(\b(?:walker|node|edge|type|ability|can)\s+)\w+/i,
-        lookbehind: true
-    },
-    'keyword': /\b(?:walker|node|edge|graph|spawn|with|entry|exit|disengage|take|ignore|visit|revisit|type|has|can|report|by|anchor|private|public|import|include|for|while|if|else|elif|skip|strict|hybrid|async|sync|test|assert|global|yield|def|return)\b/,
-    'builtin': /\b(?:int|str|list|dict|bool|float|object|edge|node|walker|true|false|null|uuid|context|info|details|bool|dict|list|int|str|edge|node|walker)\b/,
-    'boolean': /\b(?:true|false)\b/,
-    'function': /\b\w+(?=\()/,
-    'number': /(?:\b\d+\.?\d*|\B\.\d+)(?:e[+-]?\d+)?/i,
-    'operator': /[<>]=?|[!=]=?=?|--?|\+\+?|&&?|\|\|?|[?*/~^%]/,
-    'punctuation': /[{}[\];(),.:]/
+    {
+      pattern: /#.*/,
+      greedy: true
+    }
+  ],
+
+  // Triple-quoted or single-quoted strings
+  'string': {
+    pattern: /("""|'''|("|'))(?:\\[\s\S]|(?!\1)[^\\])*\1/,
+    greedy: true
+  },
+
+  // Class-like declarations: node, edge, walker, test, etc.
+  'class-name': {
+    pattern: /\b(?:enum|obj|class|node|edge|walker|test)\s+\w+/,
+    lookbehind: false
+  },
+
+  // Keywords
+  'keyword': /\b(?:async|await|continue|entry|exit|del|assert|check|break|finally|for|from|elif|else|if|except|pass|raise|return|try|while|with|to|by|spawn|ignore|visit|disengage|lambda|priv|protect|pub|static|override|let|abs|has|case|match|impl|can|def)\b/,
+
+  // Built-in types
+  'builtin': /\b(?:str|int|float|list|tuple|set|dict|bool|bytes|any|type)\b/,
+
+  // Boolean literals
+  'boolean': /\b(?:True|False|None)\b/,
+
+  // Function names before parentheses
+  'function': /\b\w+(?=\s*\()/,
+
+  // Numbers (float, hex, binary, decimal)
+  'number': {
+    pattern: /\b(?:0[xX][0-9a-fA-F]+|0[bB][01]+|0[oO][0-7]+|\d+(?:\.\d+)?(?:[eE][+-]?\d+)?[jJ]?)\b/,
+    greedy: true
+  },
+
+  // Operators
+  'operator': /[<>]=?|[!=]=?=?|--?|\+\+?|&&?|\|\|?|[*\/%^~]|<<|>>|:=|\?|@|and|or|not|in|is/,
+
+  // Punctuation
+  'punctuation': /[{}[\];(),.:]/
 };
+
+export default Prism.languages.jac;
