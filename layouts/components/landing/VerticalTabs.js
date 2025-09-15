11c464ce5c6679dbe9298ff7fc4e36efb36b5717
RanuriG <ranurigurusinghe@gmail.com>
2025-09-15 17:19:49 +0530

    responsiveness fixed

--- layouts/components/landing/VerticalTabs.js
+++ layouts/components/landing/VerticalTabs.js
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import { Highlight} from "prism-react-renderer";
 import "../../../lib/prism-jac";
 import { CodeBlock } from "../CodeBlock";
 
